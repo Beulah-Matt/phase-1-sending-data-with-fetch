5330c1a37ed6b3a864b76bec8d751b468dff9ab9
Beulah Mathenge <beulahmathenge@gmail.com>
2022-08-25 13:08:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({
+            name, 
+            email
+          }),
+
+    })
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        return object;
+      })
+      .catch(error => {document.body.innerHTML = `Error: ${error}`;
+        console.error('Unauthorised Access', error);
+    });
+}
+submitData()
+
+
+
+
+  
+//  let data = fetch("http://localhost:3000/users")
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (user) {
+//       return data
+//     });
+
+  
+//   //const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(submitData),
+//   };
+  
+//   fetch("http://localhost:3000/users", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     });
